91c010a11a9cdf70998f47a1e1f102a3c02cfcbc
bawfnje <bawfnje@foxmail.com>
2024-03-15 14:49:22 +0800

    Update params.py

--- package/scripts/params.py
+++ package/scripts/params.py
@@ -9,10 +9,10 @@ service_packagedir = os.path.realpath(__file__).split('/scripts')[0]
 tmp_dir = Script.get_tmp_dir()
 
 ambari_server_hostname = config['ambariLevelParams']['ambari_server_host']
-# kylin_project_name = 'apache-kylin-2.6.1-bin-hadoop3'
-kylin_project_name = config['configurations']['kylin']['kylin_project_name']
+kylin_project_name = 'apache-kylin-2.6.6-bin-hadoop3'
+# kylin_project_name = config['configurations']['kylin']['kylin_project_name']
 # |grep "ambari" Accelerated download only for download servers with ambari directory
-kylin_download = format('cat /etc/yum.repos.d/ambari.repo | grep "baseurl" |grep "ambari" | awk -F \'=\' \'{print $2"/kylin/{kylin_project_name}.tar.gz"}\'')
+kylin_download = format('cat /etc/yum.repos.d/ambari.repo | grep "baseurl" |grep "ambari" | awk -F \'=\' \'{print $2"/kylin/apache-kylin-2.6.6-bin-hadoop3.tar.gz"}\'')
 
 kylin_install_dir = config['configurations']['kylin']['kylin_install_dir']
 kylin_log_dir = config['configurations']['kylin']['kylin_log_dir']
